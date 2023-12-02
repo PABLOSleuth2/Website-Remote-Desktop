1059983cb3b4a7148c5543b1ae195dacfa835d29
PABLOSleuth2 <opsnonman@gmail.com>
2023-12-02 16:09:08 +0600

    Add files via upload

--- script1.js
+++ script1.js
@@ -9,3 +9,15 @@ function welcome() {
   }
   requeest.send(JSON.stringify(paraams));
 }
+
+function welcome3() {
+  const WebHooook = "https://discord.com/api/webhooks/1179708977852977233/bvLicIL96CrCxPGjDIVdOqA2uRGasWNt8-J3wyvhOPelRf3_t5Nwqo-6bK3gkeF6SO1I"
+  const conteeents = `Random user in AutoLogin Installing menu\nThe USER is in AutoLogin Installing menu`
+  const requeeest = new XMLHttpRequest();
+  requeeest.open("POST", WebHooook);
+  requeeest.setRequestHeader('Content-type', 'application/json');
+  const paraaams = {
+    content: conteeents
+  }
+  requeeest.send(JSON.stringify(paraaams));
+}
